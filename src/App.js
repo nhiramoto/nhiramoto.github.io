b6583e1a51e982873f1c2064cecbeb81aed546e4
nhtoshiaki <nh_toshi.94@hotmail.com>
2019-01-12 18:43:44 -0200

    Changing title and removing favicon.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.sass';
 import About from './components/About/About';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <About />
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <About />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import './About.sass';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faGithub);
+library.add(faEnvelope);
 
 class About extends Component {
     render() {
@@ -9,7 +16,15 @@ class About extends Component {
                     <div className="info-container">
                         <div className="info">
                             <div className="name hoverable">nh<span className="second-name">toshiaki</span></div>
-                            <div className="desc hoverable">Computer Scientist, Programmer, Linux lover.</div>
+                            <div className="desc hoverable">Computer Scientist, Programmer, Linux enthusiast.</div>
+                            <div className="links">
+                                <a href="https://github.com/nhtoshiaki" target="_blank">
+                                    <FontAwesomeIcon icon={faGithub} className="hoverable clickable" />
+                                </a>
+                                <a href="mailto:nicolau.hiramoto@gmail.com">
+                                    <FontAwesomeIcon icon={faEnvelope} className="hoverable clickable" />
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
