f293fce708615e8bb99cfa8eee0415dfa0f40a92
nhtoshiaki <nicolau.hiramoto@gmail.com>
2019-09-07 10:24:37 -0300

    Updates

---
+++ precache-manifest.b66bbc0ffade7d5e038f11090fda323d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "44e46592c377055e84c1",
+    "url": "/static/js/main.44e46592.chunk.js"
+  },
+  {
+    "revision": "834832e5794ea9056137",
+    "url": "/static/js/1.834832e5.chunk.js"
+  },
+  {
+    "revision": "44e46592c377055e84c1",
+    "url": "/static/css/main.1ab1e7f7.chunk.css"
+  },
+  {
+    "revision": "82fa4930e3c5f4b8dd68a6ca3f5e034c",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.977fe49c2590792cf81b7f408624b820.js"
+  "/precache-manifest.b66bbc0ffade7d5e038f11090fda323d.js"
 );
 
 workbox.clientsClaim();
